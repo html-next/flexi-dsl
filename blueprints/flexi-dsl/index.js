071ce7a35c41fe24907e77c8055e176778539c45
Kyle Bishop <kybishop@gmail.com>
2017-01-18 23:29:14 +0700

    chore(cleanup)

--- blueprints/flexi-dsl/index.js
+++ blueprints/flexi-dsl/index.js
@@ -5,14 +5,4 @@ module.exports = {
   afterInstall: function(options) {
     return this.addAddonToProject({ name: 'flexi-config' });
   }
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
-  // }
 };
