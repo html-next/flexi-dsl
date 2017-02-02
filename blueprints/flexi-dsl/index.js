0af3d353cfc4c71fda012db4304bb278bbf8a8d4
Robert Wagner <rwwagner90@gmail.com>
2017-02-02 10:36:58 -0500

    10

--- blueprints/flexi-dsl/index.js
+++ blueprints/flexi-dsl/index.js
@@ -3,6 +3,12 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addAddonToProject({ name: '@html-next/flexi-config', target: '2.0.0-beta.6' });
+    return this.addAddonToProject({
+      name: '@html-next/flexi-config',
+      target: '2.0.0-beta.6',
+      blueprintOptions: {
+        save: true
+      }
+    });
   }
 };
