1fbe37a6a7f4b7e91130647c087d7d18057b64ea
Kyle Bishop <kybishop@gmail.com>
2017-10-28 19:29:05 -0700

    chore(bump deps) ember 2.16, etc.

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,12 @@ module.exports = {
     'arrow-parens': ['error', 'as-needed'],
     // Destructuring is not supported in node 4.8.0
     'ember-suave/prefer-destructuring': 'off',
-    'operator-linebreak': ['error', 'before', { 'overrides': { '=': 'ignore'} } ]
+    'operator-linebreak': ['error', 'before', { 'overrides': { '=': 'ignore'} } ],
+    'indent': ['error', 2, {
+      'CallExpression': { 'arguments': 'first' },
+      'FunctionDeclaration': { 'parameters': 'first'},
+      'FunctionExpression': { 'parameters': 'first'},
+      SwitchCase: 1
+    }]
   }
 };
