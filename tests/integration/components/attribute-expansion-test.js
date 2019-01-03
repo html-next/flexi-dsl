703d9fc2ab3b2678af5fd838456d421c73304ce2
Robert Wagner <rwwagner90@gmail.com>
2019-01-03 08:49:39 -0500

    Fix tests

--- tests/integration/components/attribute-expansion-test.js
+++ tests/integration/components/attribute-expansion-test.js
@@ -1,34 +1,38 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { find, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('attribute-expansion', 'Integration | Component | attribute expansion', {
-  integration: true
-});
+module('Integration | Component | attribute expansion', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  this.render(hbs`<box align="center" vertical sm="12 visible horizontal wrap" md="align=end align-self=center" lg="align-content=between"></box>`);
+  test('it renders', async function(assert) {
+    await render(
+      hbs`<box align="center" vertical sm="12 visible horizontal wrap" md="align=end align-self=center" lg="align-content=between"></box>`
+    );
 
-  let box = this.$().find('box').get(0);
+    let box = find('box');
 
-  assert.equal(
-    box.tagName.toLowerCase(),
-    'box',
-    `We rendered the <box>: ${box.outerHTML}`);
-  assert.equal(
-    box.className,
-    'align-content-between-lg align-end-md align-self-center-md col-sm-12 visible-sm horizontal-sm wrap-sm flexi-vertical align-center',
-    `We rendered the right class names: ${box.outerHTML}`);
+    assert.equal(
+      box.tagName.toLowerCase(),
+      'box',
+      `We rendered the <box>: ${box.outerHTML}`);
+    assert.equal(
+      box.className,
+      'align-content-between-lg align-end-md align-self-center-md col-sm-12 visible-sm horizontal-sm wrap-sm flexi-vertical align-center',
+      `We rendered the right class names: ${box.outerHTML}`);
 
-  this.render(hbs`<hbox fit class="something"></hbox>`);
+    await render(hbs`<hbox fit class="something"></hbox>`);
 
-  let hbox = this.$().find('hbox').get(0);
+    let hbox = find('hbox');
 
-  assert.equal(
-    hbox.tagName.toLowerCase(),
-    'hbox',
-    `We rendered the <hbox>: ${hbox.outerHTML}`);
-  assert.equal(
-    hbox.className,
-    'something flexi-fit',
-    `We rendered the right class names and didn't clobber existing classes: ${hbox.outerHTML}`);
+    assert.equal(
+      hbox.tagName.toLowerCase(),
+      'hbox',
+      `We rendered the <hbox>: ${hbox.outerHTML}`);
+    assert.equal(
+      hbox.className,
+      'something flexi-fit',
+      `We rendered the right class names and didn't clobber existing classes: ${hbox.outerHTML}`);
+  });
 });
